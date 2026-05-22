3316aa27b47452531b3096b70b0d7cf5655012e7
snaptestmri <72049697+snaptestmri@users.noreply.github.com>
2026-05-21 23:20:41 -0700

    feat: add application entry point and configuration

---
+++ src/main/java/io/testseer/backend/TestseerBackendApplication.java
@@ -0,0 +1,12 @@
+package io.testseer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestseerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestseerBackendApplication.class, args);
+    }
+}
